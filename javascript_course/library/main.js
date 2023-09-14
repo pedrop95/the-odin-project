893217155e26c127b170fdc513e2cb4edd0ce0ce
Pedro <92050580+pedrop95@users.noreply.github.com>
2023-09-14 02:54:15 -0300

    Library project Improved
    The library project was improved. The form now has functionality, and
    the books added to the shelf can be removed too.

--- javascript_course/library/main.js
+++ javascript_course/library/main.js
@@ -16,9 +16,9 @@ Book.prototype = {
     }
 }
 
-const theHobbit = new Book("The Hobbit","J.R.R. Tolkien",310,true);
-
 const shelf = document.getElementById("shelf");
+
+let idCount = 0;
 
 function addNewBook(book){
     let newBook = document.createElement("div");
@@ -29,12 +29,32 @@ function addNewBook(book){
     else {
         borderColor = `border-l-amber-400`;
     }
-    let bookCode = `<div class="shadow-md w-96 h-48 rounded-lg bg-white ${borderColor} p-4  border-l-8">
+    let bookCode = `<div class="shadow-md w-96 h-48 rounded-lg bg-white ${borderColor} p-4  border-l-8 relative">
     <p class="font-semibold">Title</p>
     <p class='text-slate-500'>${book.info()}</p>
+    <div class="flex gap-4 absolute right-4 bottom-4">
+    <button id="delete-${idCount}"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><title>trash</title><path d="M10 7V16H8V7H10M12 7H14V16H12V7M8 2H14V3H19V5H18V19H17V20H5V19H4V5H3V3H8V2M6 5V18H16V5H6Z" /></svg></button>
+    </div>
 </div>`;
     newBook.innerHTML = bookCode;
     shelf.appendChild(newBook);
+    const deletebutton = document.getElementById(`delete-${idCount}`);
+    deletebutton.addEventListener('click', () => {
+        const trash = deletebutton.parentElement.parentElement;
+        trash.remove();
+    })
+    idCount++;
 }
 
-addNewBook(theHobbit);
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.querySelector('input[name="read"]:checked').value;
+    const booleanValue = read === "true" ? true : false;
+    const book = new Book(title,author,pages,booleanValue);
+    addNewBook(book);
+})
